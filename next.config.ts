706ba96876e98a45c0c48ca1e62c7a07ecb736d1
Tomato-Cultivator <63183805+Tomato-Cultivator@users.noreply.github.com>
2026-02-23 22:35:48 +1100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const basePath = process.env.PAGES_BASE_PATH || "";
+
 const nextConfig: NextConfig = {
-  /**
-   * Enable static exports.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
   output: "export",
-
-  /**
-   * Set base path. This is usually the slug of your repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/nextjs-github-pages",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-   */
+  trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true, // required for GitHub Pages
   },
+  basePath: basePath,
+  assetPrefix: basePath,
 };
 
 export default nextConfig;
